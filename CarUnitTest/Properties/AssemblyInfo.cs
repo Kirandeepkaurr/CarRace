9c642b695303496576d274972ad8ab135bef1e19
kiran <kiran@DESKTOP-RF1HK0D>
2019-08-26 11:47:13 +1200

    cmt

---
+++ CarUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CarUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CarUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("045cbb57-8723-470c-8c9d-baf95a862147")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CarUnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarRace;
+namespace TestCar
+{
+    [TestClass]
+    public class CarUnitTest
+    {
+        [TestMethod]
+        public void TestWinner()
+        {
+            string expected = "Busted" + Environment.NewLine + "bob won 120" + Environment.NewLine + "ai won 140" + Environment.NewLine;
+            MainForm obj = new MainForm(false, true, true, 50);
+            string actual = obj.unitTest();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
